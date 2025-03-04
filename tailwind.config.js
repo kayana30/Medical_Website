e621ec2066a1ffd212ea9a38a91d51c0fb8d00ce
kayana Sivayogarasa <90521483+kayana30@users.noreply.github.com>
2025-03-04 09:23:15 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'neutralSilver': '#F5F7FA',
+        'neutralDGrey': '#4D4D4D',
+        'brandPrimary': '#14A3C7',
+        'neutralGrey': '#717171',
+        'gray900': '#717171',
+      }
+    },
+  },
+  plugins: [require('flowbite/plugin')],
+}
+
+
